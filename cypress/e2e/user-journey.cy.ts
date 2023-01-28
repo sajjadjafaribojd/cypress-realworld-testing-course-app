2df47c66059cc8bb12c68240f13415450d495116
sajjadjafaribojd <sajjadjafaribojd@gmail.com>
2023-01-28 22:06:07 +0330

    Finish writing this user journey test

--- cypress/e2e/user-journey.cy.ts
+++ cypress/e2e/user-journey.cy.ts
@@ -14,6 +14,20 @@ describe("User Journey", () => {
         "eq",
         "/testing-your-first-application/installing-cypress-and-writing-our-first-test"
       )
+
+      cy.getByData("challenge-answer-0").click()
+      cy.getByData("next-lesson-button").should("exist").click()
+      cy.location("pathname").should(
+        "eq",
+        "/testing-your-first-application/setting-up-data-before-each-test"
+      )
+
+      cy.getByData("challenge-answer-0").click()
+      cy.getByData("next-lesson-button").should("exist").click()
+      cy.location("pathname").should(
+        "eq",
+        "/"
+      )
     })
   })
   
